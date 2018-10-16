d16ef895fb4f60c7965a81c7c25808530dd41bcf
Олекша <deryabino@gmail.com>
2018-10-16 16:05:51 +0400

    Добавили обработку ошибок заполнения модальной формы

--- js/popup.js
+++ js/popup.js
@@ -17,7 +17,9 @@ try {
 form.addEventListener("submit", function(evt) {
 	if (!login.value || !password.value) {
 		evt.preventDefault();
-		console.log("Нужно ввести логин и пароль");
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
 	} else {
 		if (isStorageSupport) {
 			localStorage.setItem("login", login.value);
@@ -39,6 +41,7 @@ link.addEventListener("click", function(evt) {
 closepop.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
 });
 
 window.addEventListener("keydown", function(evt) {
@@ -46,6 +49,7 @@ window.addEventListener("keydown", function(evt) {
 		evt.preventDefault();
 		if (popup.classList.contains("modal-show")) {
 			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
 		}
 	}
 });
