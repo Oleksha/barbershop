1ed8e07a78f85f83f2a03f8c9cd17c62eadbd349
Олекша <deryabino@gmail.com>
2018-10-16 13:54:13 +0400

    Помещаем фокус на поле Логин

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,12 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
 var closepop = document.querySelector(".modal-close");
+var login	= popup.querySelector("[name=login]");
 
 link.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
+	login.focus();
 });
 
 closepop.addEventListener("click", function(evt) {
