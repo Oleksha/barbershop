74d4c05fdcb3eb88166e2edf2af53929c348467c
Олекша <deryabino@gmail.com>
2018-10-16 17:16:12 +0400

    Исправили мелкие ошибки синтаксиса

--- js/popup.js
+++ js/popup.js
@@ -14,7 +14,7 @@ try {
 	isStorageSupport = false;
 }
 
-form.addEventListener("submit", function(evt) {
+form.addEventListener("submit", function (evt) {
 	if (!login.value || !password.value) {
 		evt.preventDefault();
 		popup.classList.remove("modal-error");
@@ -27,7 +27,7 @@ form.addEventListener("submit", function(evt) {
 	}
 });
 
-link.addEventListener("click", function(evt) {
+link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
 	if (storage) {
@@ -38,13 +38,13 @@ link.addEventListener("click", function(evt) {
 	}
 });
 
-closepop.addEventListener("click", function(evt) {
+closepop.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
 	popup.classList.remove("modal-error");
 });
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
 		if (popup.classList.contains("modal-show")) {
