aed571b318b0dde8b6521370b2f9f161ac3f61fb
Олекша <deryabino@gmail.com>
2018-10-16 12:54:47 +0400

    Сделали закрытие модального окна

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,13 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
+var closepop = document.querySelector(".modal-close");
 
 link.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
+});
+
+closepop.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
 });
