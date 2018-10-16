15a3876c3b5ca4f4e34bcc2a2b3c0596789fa3bc
Олекша <deryabino@gmail.com>
2018-10-16 17:35:45 +0400

    Исправили ошибки совпадения имен переменных

--- js/map.js
+++ js/map.js
@@ -1,22 +1,22 @@
-var link = document.querySelector(".contacts-button-map");
-var popup = document.querySelector(".modal-map");
-var closepop = document.querySelector(".modal-close");
+var linkmap = document.querySelector(".contacts-button-map");
+var popupmap = document.querySelector(".modal-map");
+var closemap = popupmap.querySelector(".modal-close");
 
-link.addEventListener("click", function (evt) {
+linkmap.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.add("modal-show");
+	popupmap.classList.add("modal-show");
 });
 
-closepop.addEventListener("click", function (evt) {
+closemap.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.remove("modal-show");
+	popupmap.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
-		if (popup.classList.contains("modal-show")) {
-			popup.classList.remove("modal-show");
+		if (popupmap.classList.contains("modal-show")) {
+			popupmap.classList.remove("modal-show");
 		}
 	}
 });
